8adab5538fce484c26dd2088e2a731739ea16efc
S4nches <luis-lipe@hotmail.com>
2021-04-23 10:44:04 -0300

    Starting of the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
+  const [sidebarOpen, setSideBarOpen] = useState(false)
+
+  const openSideBar = () =>{
+    setSideBarOpen(true)
+  }
+  const closeSideBar = () =>{
+    setSideBarOpen(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello World</h1>
     </div>
   );
 }
